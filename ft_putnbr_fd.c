b376b3b1213d3210ec8907c66ed3e60a0360a38e
MaryamAbaddi <maryamabaddi12@gmail.com>
2025-09-12 12:26:50 +0300

    Update ft_putnbr_fd.c

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -25,7 +25,13 @@ void	ft_putnbr_fd(int n, int fd)
 		n = -n;
 	}
 	if (n >= 10)
+	{
 		ft_putnbr_fd(n / 10, fd);
-	a = n % 10 + '0';
-	write(fd, &a, 1);
+		ft_putnbr_fd(n % 10, fd);
+	}
+	else
+	{
+		a = n + '0';
+		write(fd, &a, 1);
+	}
 }
